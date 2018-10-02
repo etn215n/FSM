7383c502dd2d51d29c98515cab1fe8f4f8a524c3
etn215n <43585438+etn215n@users.noreply.github.com>
2018-10-03 01:42:06 +0300

    Modify idle state

--- Assets/Scripts/IdleState.cs
+++ Assets/Scripts/IdleState.cs
@@ -5,25 +5,13 @@ public class IdleState : FSMState
 {
     public FSMState walkState;
 
-    private float time;
-    private float timeCounter;
-
     public override void OnStateEnter()
     {
         Debug.Log("Enter Idle");
-        time = 2f;
-        timeCounter = 0f;
     }
 
     public override void OnStateUpdate()
     {
-        if (timeCounter > time)
-        {
-            if (Input.GetKey(KeyCode.Space))
-                OnStateTransition(walkState);
-        }
-        else
-            timeCounter += Time.deltaTime;
     }
 
     public override void OnStateExit()
